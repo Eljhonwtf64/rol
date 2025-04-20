a47f95d3a61a1b6963f29ae0fef3e3433f08aa7c
Eljhonwtf64 <jhonguerra904@gmail.com>
2025-04-20 17:36:45 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -64,8 +64,8 @@ global.etiqueta = 'ⁱᵃᵐ|The Jhon ☆☆╮'
 global.moneda = '¥enes'
 global.welcom1 = '❍ Edita Con El Comando setwelcome'
 global.welcom2 = '❍ Edita Con El Comando setbye'
-global.banner = 'https://ibb.co/35RncsVQ'
-global.avatar = 'https://ibb.co/35RncsVQ'
+global.banner = 'https://tinyurl.com/5d2nv8jw'
+global.avatar = 'https://tinyurl.com/25ru293g'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
